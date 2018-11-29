c7ea57c4c2a3569b5ccdd58aef9dc546107472bb
Yannis De Cleene <yannisdcl@gmail.com>
2018-11-29 13:39:30 +0100

    Added modal status view

---
+++ PerfectUI/ModalStatusView.swift
@@ -0,0 +1,93 @@
+//
+//  ModalStatusView.swift
+//  PerfectUI
+//
+//  Created by Yannis De Cleene on 29/11/2018.
+//  Copyright © 2018 yannisdecleene. All rights reserved.
+//
+
+import UIKit
+
+public class ModalStatusView: UIView {
+    
+    @IBOutlet private weak var statusImage: UIImageView!
+    @IBOutlet private weak var headlineLabel: UILabel!
+    @IBOutlet private weak var subheadLabel: UILabel!
+    
+    let nibName = "YDCModalStatusView"
+    var contentView: UIView!
+    var timer: Timer?
+    
+    public override init(frame: CGRect) {
+        // For use in code
+        super.init(frame: frame)
+        setUpView()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        // For use in Interface Builder
+        super.init(coder: aDecoder)
+        setUpView()
+    }
+    
+    private func setUpView() {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: self.nibName, bundle: bundle)
+        self.contentView = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        addSubview(contentView)
+        
+        contentView.center = self.center
+        contentView.autoresizingMask = []
+        contentView.translatesAutoresizingMaskIntoConstraints = true
+        
+        headlineLabel.text = ""
+        subheadLabel.text = ""
+        contentView.alpha = 0.0
+    }
+    
+    public override func layoutSubviews() {
+        self.layoutIfNeeded()
+        self.contentView.layer.masksToBounds = true
+        self.contentView.clipsToBounds = true
+        self.contentView.layer.cornerRadius = 10
+    }
+    
+    public func set(image: UIImage) {
+        self.statusImage.image = image
+    }
+    
+    public func set(header: String) {
+        self.headlineLabel.text = header
+    }
+    
+    public func set(subheader: String) {
+        self.subheadLabel.text = subheader
+    }
+    
+    public override func didMoveToSuperview() {
+        self.contentView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+        UIView.animate(withDuration: 0.15, animations: {
+            self.contentView.alpha = 1.0
+            self.contentView.transform = CGAffineTransform.identity
+        }) { _ in
+            self.timer = Timer.scheduledTimer(
+                timeInterval: TimeInterval(3.0),
+                target: self,
+                selector: #selector(self.removeSelf),
+                userInfo: nil,
+                repeats: false)
+        }
+    }
+    @objc private func removeSelf() {
+        UIView.animate(
+            withDuration: 0.15,
+            animations: {
+                self.contentView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+                self.contentView.alpha = 0.0
+        }) { _ in
+            self.removeFromSuperview()
+        }
+    }
+    
+}
+
